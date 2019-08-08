bdea6a7afc43adad187ade025549bf032459041c
scratchglory <adellakenn@gmail.com>
2019-08-08 09:50:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
-  # Write your code here!
+	# Write your code here!
+	get '/' do
+		# create a GET route at the / URL
+		erb :index # respond to HTTP GET requests to / by rendering the designated ERB template or HTML in views/index.erb
+	end
 
-end
+	# New route ot respond to the form submission
+	post '/greet' do
+		erb :greet
+	end
+end # end of Class
--- config.ru
+++ config.ru
@@ -1,1 +1,8 @@
 # Write your code here!
+require 'sinatra'
+
+require_relative './app'
+
+run Application
+
+# define our app -- Rack::Builder.parse_file('config.ru').first
